ac6e1b25f25b3485970b300a5d24fd7cf43b5fc4
neeraj-gs <gsneeraj05@gmail.com>
2023-08-27 20:07:31 +0530

    Created a Skeleton and added basic TCSS styles for teh Video Card

--- youtube-clone/src/components/VideoCard.tsx
+++ youtube-clone/src/components/VideoCard.tsx
@@ -1,5 +1,19 @@
 export function VideoCard(){
     return <div>
-        Video Card
+        <img className="rounded-xl" src="" alt="thumbnail photo" />
+        <div className="grid grid-cols-12 pt-2">
+            <div className="col-span-1">
+                <img className="rounded-full w-20 h-20" src="" alt="YT Channel Photo" />
+            </div>
+            <div className="col-span-11 pl-5">
+                Text Here
+            </div>
+            <div className="col-span-11 text-gray-400 text-base">
+                Title Name YT Channel
+            </div>
+            <div className="col-span-11 text-gray-400 text-base">
+                Views 
+            </div>
+        </div>
     </div>
 }
