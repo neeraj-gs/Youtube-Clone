527a2fb9dbe00200e26587976b6ab44bab35ac1a
neeraj-gs <gsneeraj05@gmail.com>
2023-08-27 20:12:46 +0530

    Added props to take all the inputs from the htlm element sfrom index,tsx

--- youtube-clone/src/components/VideoCard.tsx
+++ youtube-clone/src/components/VideoCard.tsx
@@ -1,18 +1,18 @@
-export function VideoCard(){
+export function VideoCard(props:any){
     return <div>
-        <img className="rounded-xl" src="" alt="thumbnail photo" />
+        <img className="rounded-xl" src={props.vidImg} alt="thumbnail photo" />
         <div className="grid grid-cols-12 pt-2">
             <div className="col-span-1">
-                <img className="rounded-full w-20 h-20" src="" alt="YT Channel Photo" />
+                <img className="rounded-full w-20 h-20" src={props.ytImg} alt="YT Channel Photo" />
             </div>
             <div className="col-span-11 pl-5">
-                Text Here
+                {props.title}
             </div>
             <div className="col-span-11 text-gray-400 text-base">
-                Title Name YT Channel
+                {props.ytchannel}
             </div>
             <div className="col-span-11 text-gray-400 text-base">
-                Views 
+                {props.views}
             </div>
         </div>
     </div>
